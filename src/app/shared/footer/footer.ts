add152422a77dfb70f21b14c7b7335dd69aa71ab
ana jully de souza <anajullydesouza07@gmail.com>
2026-08-21 08:44:05 -0300

    roda pe  do site

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css',
+  standalone: true,
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
 })
-export class Footer {
+export class FooterComponent {
 
 }

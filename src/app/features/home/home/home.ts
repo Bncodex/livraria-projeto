6c1d7808a5c1aff8d2adc3427e0879fab3430238
ana jully de souza <anajullydesouza07@gmail.com>
2026-08-21 12:02:23 -0300

    roda pe do site atualizado

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,17 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+
 @Component({
   selector: 'app-home',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
 export class Home {
+
   readonly destaques = [
-    { titulo: 'A biblioteca da meia-noite', autor: 'Matt Haig', cor: 'midnight', tag: 'Mais vendido' },
-    { titulo: 'O sol é para todos', autor: 'Harper Lee', cor: 'sunset', tag: 'Clássico' },
-    { titulo: 'Torto Arado', autor: 'Itamar Vieira Junior', cor: 'earth', tag: 'Nacional' },
-    { titulo: 'Orgulho e Preconceito', autor: 'Jane Austen', cor: 'rose', tag: 'Romance' },
+    {
+      titulo: 'A biblioteca da meia-noite',
+      autor: 'Matt Haig',
+      cor: 'midnight',
+      tag: 'Mais vendido'
+    },
+    {
+      titulo: 'O sol é para todos',
+      autor: 'Harper Lee',
+      cor: 'sunset',
+      tag: 'Clássico'
+    },
+    {
+      titulo: 'Torto Arado',
+      autor: 'Itamar Vieira Junior',
+      cor: 'earth',
+      tag: 'Nacional'
+    },
+    {
+      titulo: 'Orgulho e Preconceito',
+      autor: 'Jane Austen',
+      cor: 'rose',
+      tag: 'Romance'
+    }
   ];
+
 }

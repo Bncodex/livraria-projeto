b5d26aa1317cb8469a55bfd03a7bc62d3bbdcb83
Yasmin Ribeiro da Conceição <yasminribeiiro06@gmail.com>
2026-08-24 09:41:43 -0300

    alteracoes sobre nos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { SobreNosComponent } from './features/sobre-nos/sobre-nos/sobre-nos';
+import { authGuard } from './core/guards/auth.guard';
+
 
 export const routes: Routes = [
   
@@ -11,15 +13,26 @@ export const routes: Routes = [
   {
     path: 'comprar',
     loadComponent: () => import('./features/comprar/compras/compras').then((m) => m.Compras),
+    canActivate: [authGuard],
   },
 
+   
   {
     path: 'sobre-nos',
     loadComponent: () => import('./features/sobre-nos/sobre-nos/sobre-nos').then(m => m.SobreNosComponent),
   },
 
-   {
+  {
+    path: 'checkout',
+    loadComponent: () => import('./features/checkout/checkout').then((m) => m.Checkout),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+  },
+  {
     path: '**',
-    redirectTo: '',
+    loadComponent: () => import('./features/pagina-nao-encontrada/pagina-nao-encontrada').then((m) => m.PaginaNaoEncontrada),
   },
 ];
---
+++ src/app/features/pagina-nao-encontrada/pagina-nao-encontrada.ts
@@ -0,0 +1,5 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({ selector: 'app-pagina-nao-encontrada', imports: [RouterLink], templateUrl: './pagina-nao-encontrada.html', styleUrl: './pagina-nao-encontrada.css' })
+export class PaginaNaoEncontrada {}
